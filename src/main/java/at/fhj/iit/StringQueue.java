aa7246504bc580889a21b074ee56295a66715a94
Eva-Maria Kienzl <eva-maria.kienzl@fh-joanneum.at>
2019-05-31 14:07:27 +0200

    Added JavaDoc descriptions of methods and class

--- src/main/java/at/fhj/iit/StringQueue.java
+++ src/main/java/at/fhj/iit/StringQueue.java
@@ -4,19 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
-// there's some Bugs included, try to debug the code and fix the Bugs
-// there are different Bugs, wrong implementation, typos, ...
-// write Test-Cases (read Queue Interface for understanding methods) and use Debugging possibilities of your IDE
+/**
+ * The queue is implemented as an array list, but has a defined max size.
+ * The methods allow to add and remove elements from the queue (with or without deletion)
+ * remove() and element() methods throw exceptions if the queue is empty. Some other methods return null in that case.
+ */
 
 public class StringQueue implements Queue {
-	
+
 	private List<String> elements = new ArrayList<String>();
 	private int maxSize = 5;
 
+	/**
+	 * Construtor of a String queue
+	 * @param maxSize defines the maximum number of elements
+	 */
 	public StringQueue(int maxSize){
 		this.maxSize = maxSize;
 	}
 
+	/**
+	 * Adds the element obj to the queue.
+	 * @param obj is added to the String queue
+	 * @return true if addition is successful, else false
+	 */
 	@Override
 	public boolean offer(String obj) {
 		if(elements.size()!= maxSize) {
@@ -27,7 +38,10 @@ public class StringQueue implements Queue {
 		}
 	}
 
-	@Override
+	/**
+	 *  Returns the first element and also deletes it.
+	 * 	@return first element or null if no element exists
+	 */
 	public String poll() {
 		String element = peek();
 
@@ -38,7 +52,11 @@ public class StringQueue implements Queue {
 		return element;
 	}
 
-	@Override
+	/**
+	 * Returns and deletes the first element with NoSuchElementException
+	 * If the queue is empty the method throws the exception.
+	 * @return first element
+	 */
 	public String remove() {
 		String element = poll();		
 
@@ -48,7 +66,10 @@ public class StringQueue implements Queue {
 		return element;
 	}
 
-	@Override
+	/**
+	 * Returns the first element without deletion
+	 * @return first element or null if the queue is empty.
+	 */
 	public String peek() {
 		String element;
 		if(elements.size() > 0)
@@ -59,7 +80,11 @@ public class StringQueue implements Queue {
 		return element;
 	}
 
-	@Override
+	/**
+	 * Returns the first element without deletion, but with NoSuchElementException
+	 * If the queue is empty the method throws the exception.
+	 * @return first element
+	 */
 	public String element() {
 		String element = peek();
 		if(element == null)
