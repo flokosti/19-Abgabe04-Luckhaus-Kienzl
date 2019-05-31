8133ffb21138dc77ba5653bef3141147b7aca3ff
Eva-Maria Kienzl <eva-maria.kienzl@fh-joanneum.at>
2019-05-31 15:19:22 +0200

    Added JUnit tests and JavaDoc comments

--- src/test/java/at/fhj/iit/StringQueueTest.java
+++ src/test/java/at/fhj/iit/StringQueueTest.java
@@ -1,42 +1,77 @@
 package at.fhj.iit;
-
-
 import org.junit.*;
+
+/**
+ * Test of String queue
+ * Methods and variables are tested via JUnit tests.
+ */
 
 public class StringQueueTest {
     StringQueue testQueue;
 
+    /**
+     * Builds a String queue with five elements
+     */
     @Before
     public void setup()
     {
         testQueue = new StringQueue(5);
     }
 
+    /**
+     *
+     */
     @Test
     public void addElements() {
 
     }
 
+    /**
+     * Tests if it is possible to add the maximum of elements and true returned.
+     */
     @Test
-    public void testOfferToMaxSize() {
+    public void testOfferReturnValues() {
+        testQueue.offer("T1");
+        testQueue.offer("Testung2");
+        testQueue.offer("Elemenetesgjhaldkjhf ehsdnfvksa3");
+        testQueue.offer("4");
 
+        Assert.assertTrue(testQueue.offer("Final5"));
     }
 
+    /**
+     * Tests whether false ist returned when offering more elements than allowed by maxSize
+     */
 
     @Test
-    public void testOfferOverflow( ) {
+    public void testOfferOverflow() {
+        testQueue.offer("Ich");
+        testQueue.offer("habe");
+        testQueue.offer("hier");
+        testQueue.offer("viel");
+        testQueue.offer("Spaß");
 
-
+        Assert.assertFalse(testQueue.offer("."));
     }
 
-
+    /**
+     * Tests if two elements that are peeked after each other are the same
+     */
     @Test
     public void doublePeek() {
+        testQueue.offer("Element1");
 
+        Assert.assertEquals(testQueue.peek(), testQueue.peek(), "Element1");
     }
 
+    /**
+     * Tests if peek on empty queue gets null as result
+     */
     @Test
-    public void peekOnEmptyQueue() {
+    public void testPeekUnderflow() {
+        String test = testQueue.peek();
+
+        Assert.assertTrue(test == null);
 
     }
 
