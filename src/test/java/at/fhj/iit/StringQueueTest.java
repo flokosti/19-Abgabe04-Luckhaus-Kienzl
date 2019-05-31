1b3e4c1b24936d9320c41e5f3d40769b34ca672d
Pascal Luckhaus <pascal.luckhaus@edu.fh-joanneum.at>
2019-05-31 14:53:22 +0200

    Add unit test sceletons

--- src/test/java/at/fhj/iit/StringQueueTest.java
+++ src/test/java/at/fhj/iit/StringQueueTest.java
@@ -1,11 +1,64 @@
 package at.fhj.iit;
 
 
+import org.junit.*;
+
 public class StringQueueTest {
+    StringQueue testQueue;
+
+    @Before
+    public void setup()
+    {
+        testQueue = new StringQueue(5);
+    }
+
+    @Test
+    public void addElements() {
+
+    }
+
+    @Test
+    public void testOfferToMaxSize() {
+
+    }
 
 
-    /**
-     * TODO Write your own tests
-     */
+    @Test
+    public void testOfferOverflow( ) {
+
+
+    }
+
+
+    @Test
+    public void doublePeek() {
+
+    }
+
+    @Test
+    public void peekOnEmptyQueue() {
+
+    }
+
+    @Test
+    public void doubleElement() {
+
+    }
+
+    @Test
+    public void elementOnEmptyQueue() {
+
+    }
+
+    @Test
+    public void testPollUnderflow() {
+
+    }
+
+    @Test
+    public void testRemoveUnderflow() {
+
+    }
+
 
 }
